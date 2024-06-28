1658d900d40427f0ce8ed82e47c850845fdf2368
Mukul Joshi <joshi.mukul.mj@gmail.com>
2024-06-28 23:06:40 +0530

    first commit for backend

---
+++ src/main/java/com/joshi/repositories/TaskRepositries.java
@@ -0,0 +1,27 @@
+package com.joshi.repositories;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.joshi.dto.TaskDto;
+import com.joshi.entities.Task;
+
+@Repository
+public interface TaskRepositries extends JpaRepository<Task, Long>{
+
+	List<Task> findAllByTitleContaining(String title);
+
+	List<Task>  findAllByUserId(Long id);
+
+	Map<String, Long> countTasksByPriority();
+
+	Map<String, Long> countTasksByStatus();
+
+	Map<String, Long> countTasksByPriorityForUser(Long id);
+
+	Map<String, Long> countTasksByStatusForUser(Long id);
+
+}
